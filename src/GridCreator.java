9140b82bf2ce4e5529476f9a76cff302202e912a
IbrahimYunuslu <ibrahimyunuslu@gmail.com>
2019-04-13 18:58:49 +0400

    adding GridCreator class

---
+++ src/GridCreator.java
@@ -0,0 +1,68 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+
+import javax.imageio.ImageIO;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+
+public class GridCreator extends JPanel {
+	private static final long serialVersionUID = 1L;
+	private BufferedImage gridImage = null;
+	private Object[][] gridArray;
+	private Boat[] shipArray;
+	private JPanel[] panelArray;
+	private JButton endSetup, randomizeBoatsBtn;
+	private JFrame window;
+	private volatile boolean setupOver = false;
+	public static boolean currentlyPlacingBoat = false;
+
+	public GridCreator(Boat[] shipArray, JFrame app) {
+		setLayout(null);
+		setBackground(Color.white);
+		setLocation(0,0);
+		window = app;
+
+		Object[][] grid = new Object[10][10];
+		for (int i = 0; i < grid.length; i++)
+			for (int j = 0; j < grid[i].length; j++)
+				grid[i][j] = 1;
+		gridArray = grid;
+		this.shipArray = shipArray;
+		panelArray = new JPanel[shipArray.length];
+		try {
+			gridImage = ImageIO.read(new File("gridLabels.png"));
+		} catch (IOException e) {
+			System.out.println("Failed to load image");
+		}
+	}
+		
+	public Object[][] getGridArray() {
+		return gridArray;
+	}
+
+	public boolean isSetupOver() {
+		return setupOver;
+	}
+
+	public JButton getButton() {
+		return endSetup;
+	}
+
+}
