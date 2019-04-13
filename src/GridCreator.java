50a19dd93e7fd219a2b0f3435e41335a9d2b543e
IbrahimYunuslu <ibrahimyunuslu@gmail.com>
2019-04-13 23:43:34 +0400

    adding setup

--- src/GridCreator.java
+++ src/GridCreator.java
@@ -52,6 +52,135 @@ public class GridCreator extends JPanel {
 			System.out.println("Failed to load image");
 		}
 	}
+	
+	public void setup() {
+		int largestBoatSize = 0;
+		for (int i = 0; i < shipArray.length; i++){
+			int temp = shipArray[i].getPlayerBoats().length;
+			if (temp > largestBoatSize)	largestBoatSize = temp;
+		}
+		
+		int windowWidth = 54 + ((47 + 5) * gridArray.length) + (2 * 5) + 50 + ((largestBoatSize + 1) * 47);
+		int windowHeight = 56 + ((47 + 5) * (gridArray.length + 1));
+		if (windowHeight < 2 * 47 + (shipArray.length * (47 + 5 + 2)))	windowHeight = 2 * 47 + (shipArray.length * (47 + 5 + 2));
+		window.setPreferredSize(new Dimension(windowWidth, windowHeight));
+		window.setMinimumSize(new Dimension(windowWidth, windowHeight));
+		window.pack();
+		setSize(window.getContentPane().getSize());
+
+		JLabel gridLabel = new JLabel(new ImageIcon(gridImage));
+		gridLabel.setSize(54 + gridArray.length + 1 + ((47 + 5) * gridArray.length), 56 + gridArray.length + 1 + ((47 + 5) * (gridArray.length)));
+		gridLabel.setLocation(0, 0);
+		gridLabel.setHorizontalAlignment(SwingConstants.LEFT);
+		gridLabel.setVerticalAlignment(SwingConstants.TOP);
+		add(gridLabel);
+
+		int buttonXPos = gridLabel.getWidth();
+
+		randomizeBoatsBtn = new JButton("Random placing");
+		randomizeBoatsBtn.setBounds(buttonXPos, 0, window.getWidth() - buttonXPos-150, 42);//buttonXPos, 0, window.getWidth() - buttonXPos, 47 - 25
+		randomizeBoatsBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Random rand = new Random();
+				for (int i = 0; i < panelArray.length; i++)	panelArray[i].setLocation(shipArray[i].getStartingOffGridPosition());
+				for (int i = 0; i < panelArray.length; i++) {
+					int timeout = 0;
+					while (timeout < 500 && shipArray[i].getStartingOffGridPosition().equals(panelArray[i].getLocation())) {
+						int x = rand.nextInt(gridArray.length);
+						int y = rand.nextInt(gridArray.length);
+						timeout++;
+						leftClick(i, x, y);
+						if (rand.nextInt(2) == 0 && !shipArray[i].getStartingOffGridPosition().equals(panelArray[i].getLocation())) rightClick(i, x, y);
+					}
+				}
+			}
+		});
+		add(randomizeBoatsBtn);
+		randomizeBoatsBtn.setVisible(true);
+		repaint();
+
+		endSetup = new JButton("Done");
+		endSetup.setBounds(buttonXPos, 42, window.getWidth() - buttonXPos-150, 42);//buttonXPos, 47 - 5, window.getWidth() - buttonXPos, window.getHeight()
+		endSetup.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setupOver = true;
+			}
+		});
+		add(endSetup);
+		endSetup.setVisible(false);
+		
+		//----------------------------------------
+		JButton bot	= new JButton("Bot");
+		bot.setBounds(buttonXPos+150, 0, window.getWidth() - buttonXPos-150, 42);
+		add(bot);
+		bot.setVisible(true);
+		
+		JButton multi = new JButton("Multiplayer");
+		multi.setBounds(buttonXPos+150, 42, window.getWidth() - buttonXPos-150, 42);
+
+		add(multi);
+		multi.setVisible(true);
+		//----------------------------------------
+
+		for (int j = 0; j < shipArray.length; j++) {
+			final int shipNum = j;
+			
+			JPanel panel = new JPanel();
+			panel.setBackground(Color.WHITE);
+			panel.setOpaque(false);
+			panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+			panel.add(Box.createRigidArea(new Dimension(0, 0)));
+
+			for (int i = 0; i < shipArray[j].getPlayerBoats().length; i++) {
+				ImageIcon icon = new ImageIcon(shipArray[j].getPlayerBoats()[i].getBoatImage());
+				JLabel label = new JLabel(icon);
+				panel.add(label);
+				panel.add(Box.createRigidArea(new Dimension(5 + 2, 0)));
+			}
+			panel.setLocation(54 + ((47 + 5) * gridArray.length) + (2 * 5) + 50, 47 + 5 + 2 + (j * (47 + 5 + 2)));
+			panel.setSize(shipArray[j].getPlayerBoats().length * (47 + 5), 47);
+			shipArray[shipNum].setStartingOffGridPosition(panel.getLocation());
+			add(panel);
+			panelArray[j] = panel;
+			setComponentZOrder(panel, 0);
+
+			panel.addMouseMotionListener(new MouseMotionAdapter() {
+				@Override
+				public void mouseDragged(MouseEvent e) {
+					if (SwingUtilities.isLeftMouseButton(e)) {
+						JPanel component = (JPanel) e.getComponent().getParent().getParent();
+						Point pt = new Point(SwingUtilities.convertPoint(e.getComponent(), e.getPoint(), component));
+						panel.setLocation((int) pt.getX() - (47 / 2), (int) pt.getY() - (47 / 2));
+						currentlyPlacingBoat = true;
+					}
+				}
+
+			});
+			panel.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseReleased(MouseEvent e) {
+					JPanel component = (JPanel) e.getComponent().getParent().getParent();
+					Point pt = new Point(SwingUtilities.convertPoint(e.getComponent(), e.getPoint(), component));
+					int counter1 = 0;
+					int counter2 = 0;
+					int value = (int) pt.getX();
+					while (54 + ((47 + 5) * counter1) < value)	counter1++;
+					counter1--;
+					int value2 = (int) (pt.getY());
+					while (56 + ((47 + 5) * counter2) < value2)	counter2++;
+					counter2--;
+					if (e.getButton() == MouseEvent.BUTTON1) {
+						currentlyPlacingBoat = false;
+						leftClick(shipNum, counter1, counter2);
+					} else if (e.getButton() == MouseEvent.BUTTON3)	rightClick(shipNum, counter1, counter2);
+					endSetup.repaint();
+				}
+
+			});
+		}
+	}
 		
 	private void rightClick(int shipNum, int x, int y) {
 		boolean isVertical = false;
