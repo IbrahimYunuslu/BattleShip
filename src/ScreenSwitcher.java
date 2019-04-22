f2c093d0df5d78e7bf1c329070977350e278e52c
Ali <a.mammadov1999@gmail.com>
2019-04-22 13:40:12 +0400

    adding Screen Switcher

---
+++ src/ScreenSwitcher.java
@@ -0,0 +1,18 @@
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class ScreenSwitcher {
+
+	private JPanel window;
+	private JLabel bkgImageContainer;
+	private volatile boolean isImageVisible;
+	private Grid grid;
+	private MyGrid small;
+	
+	
+}
